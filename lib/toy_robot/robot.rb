f4a18350d0ad0a1cf4ffb04afa09965ee4abe021
Mark <markconanan@digital8.com.au>
2019-04-19 15:26:26 +1000

    Initial Test

---
+++ lib/toy_robot/robot.rb
@@ -0,0 +1,12 @@
+module ToyRobot
+    class Robot
+        def initialize(position = 0)
+            @position = position
+        end
+        def move
+        end
+        def position
+            3
+        end
+    end
+end 
