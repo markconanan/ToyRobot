917299327d0d5d59e84ef3315e57d405f5d0fcac
Mark <markconanan@digital8.com.au>
2019-04-21 19:39:33 +1000

    Added Table, Simulator and Command with tests

---
+++ lib/toy_robot/command.rb
@@ -0,0 +1,17 @@
+module ToyRobot 
+  class Command
+
+    def self.process(command)
+      case command
+      when /\APLACE (?<x>\d+),(?<y>\d+),(?<direction>\w+)\Z/
+        [:place, $~[:x].to_i, $~[:y].to_i, $~[:direction]] 
+      when /\AMOVE\Z/
+        [:move] 
+      else
+        [:invalid, command] 
+      end
+    end
+
+    
+  end 
+end
